c3a51e8b8bbe5a79e335f8311b4727af120b96c5
Dhivyesh K <63943490+Dhivyno@users.noreply.github.com>
2022-10-06 21:09:53 +0800

    Create AS79.py

---
+++ AS/AS79.py
@@ -0,0 +1,9 @@
+def make_it_laugh(string):
+  string = string.replace("a", "haha")
+  string = string.replace("e", "haha")
+  string = string.replace("i", "haha")
+  string = string.replace("o", "haha")
+  string = string.replace("u", "haha")
+  print(string)
+
+make_it_laugh("hello world")
