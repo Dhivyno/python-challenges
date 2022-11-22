511bd12303188c57a46feadc19358d6ed08127dd
Dhivyesh K <63943490+Dhivyno@users.noreply.github.com>
2022-11-22 14:48:29 +0800

    Create GC26 Drunkometer.py

---
+++ GC (iGCSE level)/GC26 Drunkometer.py
@@ -0,0 +1,10 @@
+phrase = "pazazz pazazzes tazzas zaqazip pizzazzes"
+
+check1 = input(
+    "type this phrase: pazazz pazazzes tazzas zaqazip pizzazzes -  ")
+check2 = input(
+    "type this phrase again: pazazz pazazzes tazzas zaqazip pizzazzes -  ")
+if check1 != "pazazz pazazzes tazzas zaqazip pizzazzes" or check2 != "pazazz pazazzes tazzas zaqazip pizzazzes":
+    print("You are not sober enough to operate this machinery")
+else:
+    print("You are clear to operate this machinery")
