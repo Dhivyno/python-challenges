22a0fda424d8029bdb1ccfb51e3008ebfc2ddd7a
Dhivyesh K <63943490+Dhivyno@users.noreply.github.com>
2023-01-06 21:59:31 +0800

    Update A234 Bit Manipulation Solver.py

--- A2/A234 Bit Manipulation Solver.py
+++ A2/A234 Bit Manipulation Solver.py
@@ -1,18 +1,14 @@
 def set_bit(num, index):
-    """ Set the bit at the given index to 1 """
     mask = 1 << index
     return num | mask
 
 def clear_bit(num, index):
-    """ Set the bit at the given index to 0 """
     mask = 1 << index
     return num & ~mask
 
 def toggle_bit(num, index):
-    """ Toggle the bit at the given index """
     mask = 1 << index
     return num ^ mask
 
 def get_bit(num, index):
-    """ Get the value of the bit at the given index """
     return (num >> index) & 1
